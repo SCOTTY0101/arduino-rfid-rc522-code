767f1917226961ce3f124a5b0062d8e289b0ac5d
Alan George <45034425+SCOTTY0101@users.noreply.github.com>
2022-07-10 21:01:34 +1000

    Update run-code.c

--- run-code.c
+++ run-code.c
@@ -20,24 +20,24 @@ void setup()
    digitalWrite(RED, LOW);
 
 }
+
 void loop() 
 {
   // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent()) 
-  {
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
     return;
   }
+ 
   // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial()) 
-  {
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;
   }
+ 
   //Show UID on serial monitor
   Serial.print("UID tag :");
   String content= "";
   byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++) 
-  {
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
@@ -46,8 +46,9 @@ void loop()
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if ((content.substring(1) == "1A 7C 78 30") || (content.substring(1) == "CC CC A3 16") ) //change here the UID of the card/cards that you want to give access
-  {
+  if ((content.substring(1) == "1A 7C 78 30") || (content.substring(1) == "CC CC A3 16") ) {
+   //change here the UID of the card/cards that you want to give access
+  
     Serial.println("Authorized access");
     Serial.println();
     digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
